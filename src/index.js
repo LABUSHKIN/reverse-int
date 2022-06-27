c48f93cb57252b0bf3f374fbb829b791134a4b3d
Sergey Lab <3403816@gmail.com>
2022-06-27 08:59:20 +0300

    fix: code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-/*module.exports = function reverse (n) {
+module.exports = function reverse (n) {
 let Arr = toString(n);
 Arr = split.Arr('');
 let Arr1 = reverse.Arr();
-return join.Arr1;
-}*/
-module.exports = function reverse (n) {
-    return (Math.abs(n)).toString().split('').reverse().join('')
-  }
+return join.Arr1();
+}
