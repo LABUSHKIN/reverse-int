85980b9b6e0fc5d7f3d36fbb0c3e7d53cf9dce21
Sergey Lab <3403816@gmail.com>
2022-07-16 20:34:55 +0300

    fix: empty

--- src/index.js
+++ src/index.js
@@ -6,16 +6,4 @@ console.log (n);
  module.exports = function reverse (n) {
 let Arr = Math.abs(n);
 
-//let arr2 = num.toString(Arr);
-//console.log (Arr);
-let Arr1 = Arr.toString();
-//console.log(Arr1); console.log(typeof Arr1);
-let Arr2 = Arr1.split('');
-//console.log(Arr2);
-let Arr3 = Arr2.reverse();
-//console.log(Arr3);
-let Arr4 = Arr3.join('');
-//console.log(Arr);
-//console.log(typeof Arr4);
-return Arr4;
- };
+ }
