a71f23df9c27a21c683cb1fd04e75e3717834f1d
Sergey Lab <3403816@gmail.com>
2022-07-16 21:32:00 +0300

    fix: ..

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // reverse-int --- смотреть в консоли (включив все консоли) и все будет понятно
 
 let n = -991;
-console.log (n);
+console.log (n); // ... и т.д.
 
  module.exports = function reverse (n) {
 let Arr = Math.abs(n);
 
  }
+
+ // empty
